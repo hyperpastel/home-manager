900cfe2198bab0569603f9302ccb85b45796af16
hyperpastel <vmutze04@gmail.com>
2025-11-11 00:29:16 +0100

    feat(home): modularize

---
+++ hosts/darksea/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  imports = [ ../common ];
+
+  wayland.windowManager.hyprland.settings."monitor" = "HDMI-A-1,1920x1080@144,auto,1";
+
+}
---
+++ hosts/teyvat/default.nix
@@ -0,0 +1,9 @@
+{ }:
+
+{
+
+  imports = [ ../common ];
+
+  wayland.windowManager.hyprland.settings."monitor" = "eDP-1,1920x1200,auto,1";
+
+}
